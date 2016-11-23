db31bbdeb0af769d033dc6d50e69cd041b27c7a5
Fernando Bolson Dias <fernando_bolsondias@hotmail.com>
2016-11-22 23:50:59 -0200

    prodserv
    prodserv

---
+++ ProjetoFinal/uCadProdServ.pas
@@ -0,0 +1,34 @@
+unit uCadProdServ;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, FormPadrao, cxGraphics, cxControls,
+  cxLookAndFeels, cxLookAndFeelPainters, cxContainer, cxEdit, Vcl.Menus,
+  dxBarBuiltInMenu, cxStyles, cxCustomData, cxFilter, cxData,
+  cxDataStorage, cxNavigator, Data.DB, cxDBData, Data.FMTBcd, Data.SqlExpr,
+  cxClasses, Datasnap.Provider, Datasnap.DBClient, System.Actions,
+  Vcl.ActnList, System.ImageList, Vcl.ImgList, cxGridLevel,
+  cxGridCustomView, cxGridCustomTableView, cxGridTableView,
+  cxGridDBTableView, cxGrid, cxPC, Vcl.StdCtrls, cxButtons, cxGroupBox,
+  Vcl.Buttons, Vcl.ExtCtrls;
+
+type
+  TFPadraoManut1 = class(TFPadraoManut)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FPadraoManut1: TFPadraoManut1;
+
+implementation
+
+
+
+{$R *.dfm}
+
+end.
